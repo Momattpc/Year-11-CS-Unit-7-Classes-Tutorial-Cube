b49be4d25640e9cad08580f1f80c0efdd1a4a35a
moses <moseskplai@gmail.com>
2024-01-18 20:25:03 +0800

    Completed Assignment

---
+++ src/Cube.java
@@ -0,0 +1,41 @@
+public class Cube {
+    private int side;
+
+    public Cube() {
+        side = 1;
+    }
+
+    public Cube(int side) {
+        if (side < 1) {
+            throw new IllegalArgumentException("A cube's side length cannot be less than 1!");
+        } else {
+            this.side = side;
+        }
+    }
+
+    public void setSide(int side) {
+        if (side < 1) {
+            throw new IllegalArgumentException("A cube's side length cannot be less than 1!");
+        } else {
+            this.side = side;
+        }
+
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public int getSurfaceArea() {
+        return (side * side * 6);
+    }
+
+    public int getVolume() {
+        return (side * side * side);
+    }
+
+    public String returnString() {
+        return "Cube{side=" + side + "}";
+    }
+}
+
